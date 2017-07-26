7378fed803918888bd656c6a4ec1e56cef1baa79
Jonathan Lehr <jonathan@aboutobjects.com>
2017-07-26 19:08:51 -0400

    cleanup

---
+++ demos/ReadingList/ReadingList/AppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.m
+//  ReadingList
+//
+//  Created by Jonathan Lehr on 7/26/17.
+//  Copyright © 2017 About Objects. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@end
